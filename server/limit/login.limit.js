b5093c34e24a87c16c25ee6f6e799a8c73af391b
TheHackBoxCampus <millercampusdeveloper@gmal.com>
2023-09-02 08:01:37 -0500

    feat(root): Configuracion de limite de peticiones
    Se configuro el limite de peticiones para el endpoint login

---
+++ server/limit/login.limit.js
@@ -0,0 +1,15 @@
+import { rateLimit } from "express-rate-limit";  
+
+const loginLimit = rateLimit({
+    windowMs: 30 * 1000,
+    max: 5,
+    legacyHeaders: true,
+    standardHeaders: true,
+    message: (req, res) => {
+        res.status(429).send({status: 429, message: "Limite de peticiones alcanzado!"})
+    }
+})
+
+export {
+    loginLimit
+}
