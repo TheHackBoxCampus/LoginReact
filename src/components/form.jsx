b955d3ad487bffc6c0fb01a6636cc9b94ed979fb
TheHackBoxCampus <millercampusdeveloper@gmal.com>
2023-09-02 08:05:46 -0500

    feat(root): Datos de entrada del formulario
    Se creo la logica de manejo de datos de entrada en el formulario
    
    !! TIENE BUGS
    No esta terminado por ende, no funciona correctamente

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,14 +1,51 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useRef, useState} from 'react';
 import "../assets/form.css"
+import Swal from 'sweetalert2';
+import { useFormik } from 'formik';
+import { validateLoginFrontend } from '../schemas/login.[DTO_FRONTEND].js';
 
 export default function Form() {
+    const [LoggedIn, setLoggedIn] = useState(false); 
+    const emailRef = useRef(null)
+    const passwordRef = useRef(null)
+// TODO: Resolver y ajustar la logica a las necesidades.
+    const send = async () => {
+        const formik = useFormik({
+            initialValues: {
+                email: '',
+                passport: ''
+            },
+            validationSchema: validateLoginFrontend,
+            onSubmit: async (params) => {
+                let request = await (await fetch("http://localhost:5000/login", {
+                    method: "GET",
+                    body: user,
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                })).json(); 
+        
+                if(request.state >= 400) {
+                    Swal.fire({
+                        title: "Ops!, ha ocurrido un error",
+                        text: request.message,
+                        icon: "error"
+                    })
+                }else {
+                    setLoggedIn(true)
+        
+                }    
+            }
+        })
+    }
+
     return (
         <div className='container_form'>
             <form className='form'>
                 <h1>Login</h1>
-                <input type="text" placeholder='email'/>
-                <input type="password" placeholder='password'/>
-                <button>Enviar</button>
+                <input name='email' type="text" placeholder='email'/>
+                  <input name='password' type="password" placeholder='password'/>
+                <button onClick={send}>Enviar</button>
             </form>
         </div>
     )
