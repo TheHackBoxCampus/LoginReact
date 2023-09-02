8e65c3a77a2808983783e007ffd3132df3dc57b2
TheHackBoxCampus <millercampusdeveloper@gmal.com>
2023-09-02 08:02:27 -0500

    feat(root): Cambio en los resultadoes de la ruta
    Debido a datos iniciales, cambio y se instanciaron los nuevos middlewares para el correcto funcionamiento

--- server/routes/[auth].js
+++ server/routes/[auth].js
@@ -1,12 +1,14 @@
 import { Router } from "express";
-
+import { loginLimit } from "../limit/login.limit.js";
+import { dtoInstancesForLogin } from "../storage/[DTO].login.js";
+import { validateLogin } from "../middleware/login.md.js";
+import { login } from "../controllers/v1/auth.controller.js";
 /**
  * ? versions
  */
 
-
 const auth = Router(); 
 
-auth.get("/login", (req, res) => res.send("login!!!") )
+auth.get("/login", loginLimit, dtoInstancesForLogin, validateLogin, login)
 
 export default auth
