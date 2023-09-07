d1d2376711b9f7b12f99a2ead0aaef14b39d5300
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-09-07 09:00:37 -0500

    feat(root): Enrutado en el frontEnd
    Se realizo la instancia de routes para la redireccion desde el frontend, utilizando el ROUTER-DOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Form from './components/form'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import "./assets/index.css"
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Form/>
+  },
+  {
+    path: "/success",
+    element: <h1>logueado</h1>
+  } 
+])
+
 ReactDOM.createRoot(document.querySelector('#root')).render(
-  <React.StrictMode>
-    <Form/>
-  </React.StrictMode>
+    <RouterProvider router={router}/>
 )
