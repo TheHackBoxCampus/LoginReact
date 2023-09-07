b7cec7ef742501c05d4022c57814832a1c763ca4
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-09-07 09:01:31 -0500

    feat(root): Regex en el DTO
    Se agregaron expresiones regulares al DTO en la parte del FRONTEND para impedir la entrada de datos no deseados

--- src/schemas/login.[DTO_FRONTEND].js
+++ src/schemas/login.[DTO_FRONTEND].js
@@ -2,7 +2,7 @@ import * as Yup from "yup";
 
 const validateLoginFrontend = Yup.object().shape({
     email: Yup.string().email("Correo electronico incorrecto!").required("Parametro email es obligatorio"),
-    password: Yup.string().min(6, "La contraseña debe tener minimo 6 caracteres").required("Parametro password es obligatorio!")
+    password: Yup.string().min(6, "La contraseña debe tener minimo 6 caracteres").matches("^[A-Za-z0-9]+$", "contraseña invalida!").required("Parametro password es obligatorio!")
 })
 
 export {
