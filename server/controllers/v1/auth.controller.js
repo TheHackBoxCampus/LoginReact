18caa9fc327fd629744fb23af7650d83d4be5647
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-09-07 17:33:11 -0500

    feat(root): props y funciones nuevas
    se agregaron algunos cambios sobre nuevas instancias que se van a utilizar

--- server/controllers/v1/auth.controller.js
+++ server/controllers/v1/auth.controller.js
@@ -1,5 +1,14 @@
 import {conexion} from "../../config/db.js";
 
+const verifyAuth = async (id) => {
+    try {
+
+    }catch(err) {
+        console.lof(res)
+    }
+}
+
+
 const login = async (req, res) => {
     try {
         let {email, password} = req.body; 
@@ -7,7 +16,11 @@ const login = async (req, res) => {
         let users = await db.collection("user"); 
         let userIsMatch = await users.find({email, password}).toArray(); 
         if(userIsMatch.length < 1) return res.status(404).send({status: 404, message: "Credenciales incorrectas!"})
-        else return res.status(200).send({status: 200, redirectUrl: "Redireccion exitosa!!", success: true});  
+        else {
+        let permissions = verifyAuth(userIsMatch.id_user); 
+            
+        return res.status(200).send({status: 200, redirectUrl: "Redireccion exitosa!!", success: true});  
+      }
     }catch(err) {
         res.sendStatus(500);
         console.log(err); 
