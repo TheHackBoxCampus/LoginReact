aa50d46a9861791ee7b5cd6f201efd8d36d3ca21
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-09-22 08:25:55 -0500

    feat(root): Desplegar proyecto

--- server/app.js
+++ server/app.js
@@ -3,13 +3,17 @@ import cors from "cors";
 import auth from "./routes/[auth].js";
 import morgan from "morgan";
 import rutaejemplo from "./routes/example.js";
+import { fileURLToPath } from "url";
+import path from "path";
 
 const app = express(); 
 
 /** 
  * ! middlewares
  */
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const DIST_DIRECTORY = path.join(__dirname, "../dist");
 app.use(express.json());
 app.use(express.text());
 app.use(cors());
@@ -20,5 +24,6 @@ app.use(morgan("dev"))
 
 app.use(auth)
 app.use(rutaejemplo)
+app.use(express.static(DIST_DIRECTORY));
 
 export default app; 
--- vite.config.js
+++ vite.config.js
@@ -11,3 +11,4 @@ export default defineConfig({
      port: env.VITE_FRONTEND
   }
 })
+  
